d9ee3f29de8f9d56cfe5ac8f9ccdde312aa11744
Michael Frister <michael+git@frister.net>
2015-12-28 21:22:37 +0100

    Config: Allow via env vars

--- main.go
+++ main.go
@@ -5,12 +5,40 @@ import (
 	"net/http"
 
 	"github.com/carbocation/interpose"
+	"github.com/kelseyhightower/envconfig"
 )
 
 const DATA_DIR = "./data"
 const URL_PREFIX = "http://localhost:8000/"
 
+type Config struct {
+	Address   string `envconfig:"ADDRESS"`
+	DataDir   string `envconfig:"DATA_DIR"`
+	URLPrefix string `envconfig:"URL_PREFIX"`
+}
+
+var config *Config
+
+func loadConfig() *Config {
+	conf := Config{}
+	err := envconfig.Process("droplink", &conf)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if conf.Address == "" {
+		conf.Address = "localhost:8000"
+	}
+	if conf.DataDir == "" {
+		conf.DataDir = "./data"
+	}
+	if conf.URLPrefix == "" {
+		conf.URLPrefix = "http://localhost:8000/"
+	}
+	return &conf
+}
+
 func main() {
+	config = loadConfig()
 	middle := interpose.New()
 
 	router := http.NewServeMux()
@@ -19,8 +47,8 @@ func main() {
 	router.Handle("/upload", http.HandlerFunc(upload))
 	router.Handle("/", http.HandlerFunc(download))
 
-	log.Println("Listening....")
-	err := http.ListenAndServe("127.0.0.1:8000", middle)
+	log.Printf("Listening on %s", config.Address)
+	err := http.ListenAndServe(config.Address, middle)
 	if err != nil {
 		log.Fatal(err)
 	}
